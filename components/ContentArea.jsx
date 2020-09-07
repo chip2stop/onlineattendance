ec16d768c3604bbd30d848a87c2139cd0ba4a925
chip2stop <113591279+chip2stop@users.noreply.github.com>
2020-09-07 19:06:49 +0000

    Fix a small typo.

---
+++ components/ContentArea.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+
+import Link from "next/link";
+
+import { Flex, Heading, Text, IconButton, Button } from "@chakra-ui/react";
+import { FiPlus } from "react-icons/fi";
+
+import { useSelecto
