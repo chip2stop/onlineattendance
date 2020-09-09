018403098659c65c34fbf069984f148dd476f538
chip2stop <113591279+chip2stop@users.noreply.github.com>
2020-09-09 14:52:59 +0000

    Fix travis error.

--- components/ContentArea.jsx
+++ components/ContentArea.jsx
@@ -5,4 +5,11 @@ import Link from "next/link";
 import { Flex, Heading, Text, IconButton, Button } from "@chakra-ui/react";
 import { FiPlus } from "react-icons/fi";
 
-import { useSelecto
+import { useSelector, useDispatch } from "react-redux";
+import { setRevenue, setExpense } from "../store/slices/financeSlice";
+import { order } from "../store/slices/productSlice";
+
+import TableArea from "./TableArea";
+
+import { getCurrentDate, formatCurrentMonth } from "../utils/dateUtil";
+impo
