5bb8e65ceeaa62842b92432b2b6eef4289f7814b
chip2stop <113591279+chip2stop@users.noreply.github.com>
2020-09-22 05:32:01 +0000

    Updated text

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,3 +1,14 @@
 import Link from "next/link";
 
-import { Flex, Heading, Avatar, Text, Icon }
+import { Flex, Heading, Avatar, Text, Icon } from "@chakra-ui/react";
+import { FiHome, FiCoffee, FiGithub } from "react-icons/fi";
+
+export default function Menu() {
+  return (
+    <Flex
+      w="15%"
+      flexDir="column"
+      alignItems="center"
+      backgroundColor="#020202"
+      color="#fff"
+    >
