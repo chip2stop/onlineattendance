59c86ee41ed1568f8281a43b6c320aed36b082f0
chip2stop <113591279+chip2stop@users.noreply.github.com>
2020-10-14 08:35:53 +0000

    Blacken

--- hooks/useMask.jsx
+++ hooks/useMask.jsx
@@ -1,1 +1,10 @@
-export const useMa
+export const useMask = () => ({
+  toBRL: (value) => {
+    const formattedNumber = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value);
+
+    return formattedNumber;
+  },
+});
