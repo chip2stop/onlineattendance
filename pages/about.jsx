ca89fc6a60f9aea96d5181e23ae7e41bb846a3e9
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-01-13 14:40:44 +0000

    Pontoon: Update Slovak (sk) localization of Test Pilot Website

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,4 +13,16 @@ export default function About() {
       </Head>
       <Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
         {/* Column 1 - Menu */}
- 
+        <Menu />
+
+        {/* Column 2 - ContentArea */}
+        <Flex
+          w="85%"
+          p="3%"
+          flexDir="column"
+          overflow="auto"
+          minH="100vh"
+          background="#fff"
+        >
+          <Heading
+            textAli
