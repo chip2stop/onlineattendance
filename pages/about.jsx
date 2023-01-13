87c19e69510112d8f320ecccfd3d14c635ed097f
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-01-13 11:06:14 +0000

    remove blog

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,4 +3,14 @@ import Head from "next/head";
 
 import Menu from "../components/Menu";
 
-import { Flex, Heading, Stac
+import { Flex, Heading, Stack, Text, Box, Image } from "@chakra-ui/react";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Stock Management</title>
+      </Head>
+      <Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
+        {/* Column 1 - Menu */}
+ 
