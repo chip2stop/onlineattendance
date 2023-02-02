e15edf7de4c5e2489a4b09ae3532cfbe4508975c
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-02-02 21:38:06 +0000

    updated by automatic grading script

--- pages/product/[productId].jsx
+++ pages/product/[productId].jsx
@@ -1,1 +1,12 @@
-import React, { useState, useEffect } 
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { edit } from "../../store/slices/productSlice";
+
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Menu from "../../components/Menu";
+
+import {
+  Flex,
+  FormCo
