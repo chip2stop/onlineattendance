0cdf1e325a1703048f155c7c00b1c754e9eff3ea
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-02-08 08:19:15 +0000

    Replace deprecated methods

--- pages/product/[productId].jsx
+++ pages/product/[productId].jsx
@@ -22,3 +22,13 @@ import {
 export default function Product() {
   const [productName, setProductName] = useState("");
   const [productCategory, setProductCategory] = useState("");
+  const [productValue, setProductValue] = useState("");
+
+  const state = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+
+  const router = useRouter();
+  const { query } = useRouter();
+
+  useEffect(() => {
+    // loop to check if the id passed as a parameter
