13c608904a94909a24f6c683fa8dfba1087eeb4b
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-02-25 13:39:55 +0000

    Create CONTRIBUTE.md

--- store/slices/financeSlice.jsx
+++ store/slices/financeSlice.jsx
@@ -1,4 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const slice = createSlice({
-  name
+  name: "finance",
+  initialState: {
+    revenue: 0,
+    expense: 0,
+  },
+  reducers: {
+    setRevenue: (state, action) => {
+      return { ...state, revenue: action.payload };
+    },
+    setExpense: (state, action) => {
+      return { ...state, expense: action.payloa
