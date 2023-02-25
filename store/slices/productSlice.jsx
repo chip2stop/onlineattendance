e43ff756325462d32ff23eb5a51a68a5f4a69d3a
chip2stop <113591279+chip2stop@users.noreply.github.com>
2023-02-25 18:17:20 +0000

    Add some TODOs.

--- store/slices/productSlice.jsx
+++ store/slices/productSlice.jsx
@@ -3,4 +3,14 @@ import { v4 as uuid } from "uuid";
 
 import data from "../../data/product";
 
-const slice = crea
+const slice = createSlice({
+  name: "product",
+  initialState: data,
+  reducers: {
+    insert: (state, action) => {
+      if (action.payload.productName) {
+        let newState = [...state];
+        newState.push({
+          id: uuid(),
+          productName: action.payload.productName,
+          category: actio
